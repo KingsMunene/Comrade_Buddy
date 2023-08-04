a129c7128c8d8cabeadd554b9290c07f0eddc2b4
KingsMunene <munenekings2@gmail.com>
2023-08-04 21:45:19 +0300

    GoogleAuthUiClient class

---
+++ app/src/main/java/com/example/comradebuddy/presentation/sign_in/GoogleAuthUiClient.kt
@@ -0,0 +1,12 @@
+package com.example.comradebuddy.presentation.sign_in
+
+import android.content.Context
+import com.google.android.gms.auth.api.identity.SignInClient
+
+class GoogleAuthUiClient(
+    private val context: Context,
+    private val oneTapClient: SignInClient
+
+    ) {
+
+}
