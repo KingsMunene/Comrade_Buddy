d77427c6499491eea5f5ab4f47b95626e79e7f86
KingsMunene <munenekings2@gmail.com>
2023-08-05 13:04:30 +0300

    Auth client files

--- app/src/main/java/com/example/comradebuddy/presentation/sign_in/GoogleAuthUiClient.kt
+++ app/src/main/java/com/example/comradebuddy/presentation/sign_in/GoogleAuthUiClient.kt
@@ -1,12 +1,106 @@
 package com.example.comradebuddy.presentation.sign_in
 
 import android.content.Context
+import android.content.Intent
+import android.content.IntentSender
+import com.example.comradebuddy.R
+import com.google.android.gms.auth.api.identity.BeginSignInRequest
+import com.google.android.gms.auth.api.identity.BeginSignInRequest.GoogleIdTokenRequestOptions
 import com.google.android.gms.auth.api.identity.SignInClient
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.tasks.await
+import java.util.concurrent.CancellationException
 
+
+// Class to signin to signOut and to get the user information
 class GoogleAuthUiClient(
     private val context: Context,
+    //This will show us the dialog to sign in
     private val oneTapClient: SignInClient
-
     ) {
+
+    //Auth Object
+    private val auth = Firebase.auth
+
+    // Sign in suspend function
+    suspend fun signIn(): IntentSender?{
+        val result = try {
+            oneTapClient.beginSignIn(
+                buildSignInRequest()
+            ).await()
+
+        } catch (e: Exception) {
+            e.printStackTrace()
+            if (e is CancellationException) throw e
+            null
+        }
+        return result?.pendingIntent?.intentSender
+    }
+
+    // Get the results for firebase
+
+    suspend fun signInWithIntent(intent: Intent): SignInResult{
+        val credential = oneTapClient.getSignInCredentialFromIntent(intent)
+        val googleIdToken = credential.googleIdToken
+        val googleCredentials = GoogleAuthProvider.getCredential(googleIdToken, null)
+        return try {
+            val user = auth.signInWithCredential(googleCredentials).await().user
+            SignInResult(
+                data = user?.run {
+                    UserData(
+                        userId = user.uid,
+                        userName = user.displayName,
+                        profilePictureUrl = photoUrl?.toString()
+                        )
+                },
+                errorMessage = null
+            )
+
+        } catch (e: Exception) {
+            if (e is CancellationException) throw e
+            SignInResult(
+                data = null,
+                errorMessage = e.message
+            )
+        }
+
+    }
+
+    //SignOut function
+    suspend fun signOut(){
+        try {
+
+            oneTapClient.signOut().await()
+            auth.signOut()
+
+        } catch (e: Exception) {
+            e.printStackTrace()
+            if (e is CancellationException) throw e
+        }
+
+    }
+
+    fun getSignedInUser(): UserData? = auth.currentUser?.run{
+        UserData(
+            userId = uid,
+            userName = displayName,
+            profilePictureUrl = photoUrl?.toString()
+        )
+    }
+
+    private fun buildSignInRequest(): BeginSignInRequest {
+        return BeginSignInRequest.Builder()
+            .setGoogleIdTokenRequestOptions(
+                GoogleIdTokenRequestOptions.builder()
+                    .setSupported(true)
+                    .setFilterByAuthorizedAccounts(false)
+                    .setServerClientId(context.getString(R.string.web_client_id))
+                    .build()
+            )
+            .setAutoSelectEnabled(true)
+            .build()
+    }
 
 }
