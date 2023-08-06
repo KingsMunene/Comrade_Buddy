7489bb693b7b84e6266e77935313838578aebe00
KingsMunene <munenekings2@gmail.com>
2023-08-06 23:48:02 +0300

    LazyGrid height

--- app/src/main/java/com/example/comradebuddy/presentation/notes/CourseList.kt
+++ app/src/main/java/com/example/comradebuddy/presentation/notes/CourseList.kt
@@ -2,12 +2,14 @@ package com.example.comradebuddy.presentation.notes
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.height
 
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -23,8 +25,9 @@ fun CoursesList(
     LazyVerticalGrid(
         columns = GridCells.Fixed(2),
         contentPadding = PaddingValues(vertical = 16.dp),
-        verticalArrangement = Arrangement.spacedBy(8.dp),
-        horizontalArrangement = Arrangement.spacedBy(10.dp)
+        horizontalArrangement = Arrangement.spacedBy(6.dp),
+        verticalArrangement = Arrangement.spacedBy(6.dp),
+        modifier = Modifier.height(300.dp)
     )
     {
         items(
