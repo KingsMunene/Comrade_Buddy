17d359d94ffa8609a241bdf8c96839a45ba9fb19
KingsMunene <munenekings2@gmail.com>
2023-08-06 23:51:14 +0300

    comment

--- app/src/main/java/com/example/comradebuddy/presentation/notes/PdfViewer.kt
+++ app/src/main/java/com/example/comradebuddy/presentation/notes/PdfViewer.kt
@@ -4,12 +4,12 @@ import androidx.compose.runtime.Composable
 import androidx.lifecycle.viewmodel.compose.viewModel
 
 // PDf view Composable
-
 @Composable
 fun PdfDocViewer(
     fileName: String,
     pdfViewModel: PdfViewModel = viewModel()
 ) {
+    // Call the pdfViewer composable function in the viewModel
     pdfViewModel.PdfViewer(fileName)
 
 }
