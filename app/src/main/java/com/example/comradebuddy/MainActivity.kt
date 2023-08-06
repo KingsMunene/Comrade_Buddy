4a5b9713d391a5cedd90180fbca787508dc697f0
KingsMunene <munenekings2@gmail.com>
2023-08-06 23:52:20 +0300

    Calling start screen

--- app/src/main/java/com/example/comradebuddy/MainActivity.kt
+++ app/src/main/java/com/example/comradebuddy/MainActivity.kt
@@ -3,44 +3,47 @@ package com.example.comradebuddy
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.lifecycleScope
+
+import com.example.comradebuddy.presentation.navigation.StartScreen
+
 import com.example.comradebuddy.ui.theme.ComradeBuddyTheme
 
+enum class AppNavigation{
+    SPLASH,
+    LOGIN,
+    HOME,
+    UNITS,
+    NOTES
+}
+
+
+
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ComradeBuddyTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                StartScreen(
+                    lifecycle = lifecycleScope,
+                    context = applicationContext
+                )
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ComradeBuddyTheme {
-        Greeting("Android")
+        //SignInScreen()
     }
 }
