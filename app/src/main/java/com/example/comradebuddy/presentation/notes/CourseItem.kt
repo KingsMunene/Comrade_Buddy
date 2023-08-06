2fd90bc344df416ea193e8bb55a42b2a08bd8ea6
KingsMunene <munenekings2@gmail.com>
2023-08-06 23:53:45 +0300

    Redesign the course Item composable

--- app/src/main/java/com/example/comradebuddy/presentation/notes/CourseItem.kt
+++ app/src/main/java/com/example/comradebuddy/presentation/notes/CourseItem.kt
@@ -4,16 +4,23 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -25,36 +32,44 @@ fun CourseItem(
     courseName: String,
     onCourseClicked: () -> Unit
 ) {
-
-    Surface(
-        shape = MaterialTheme.shapes.small,
+    Surface (
         modifier = Modifier
-            .clickable(onClick = onCourseClicked)
-            .background(color = MaterialTheme.colorScheme.background)
-    ) {
+            .clip(MaterialTheme.shapes.small)
+            .clickable(
+                onClick = onCourseClicked
+            )
+            .padding(16.dp),
+    ){
 
-        Column (
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center,
-            modifier = Modifier
-        ){
+        Column(
+            modifier = Modifier,
+            verticalArrangement = Arrangement.Center
 
+        ) {
             Image(
                 painter = painterResource(id = R.drawable.logo),
                 contentDescription = null,
                 contentScale = ContentScale.Crop,
-                modifier = Modifier.size(100.dp)
+                modifier = Modifier
+                    .clip(MaterialTheme.shapes.medium)
+                    .aspectRatio(16 / 8f)
             )
+
+            Spacer(modifier = Modifier.height(5.dp))
+
             Text(
                 text = courseName,
-                style = MaterialTheme.typography.titleMedium,
-                modifier = Modifier.padding()
+                style = MaterialTheme.typography.titleSmall,
+                modifier = Modifier
+                    .padding(4.dp),
+                maxLines = 2
             )
         }
     }
+
 }
 
-@Preview()
+@Preview(showBackground = true)
 @Composable
 fun courseItem() {
     CourseItem(courseName = "Something") {
