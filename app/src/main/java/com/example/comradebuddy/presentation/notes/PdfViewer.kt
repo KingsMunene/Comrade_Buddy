c5e084ce9291386485bd20fe7262b16577e6dbd4
KingsMunene <munenekings2@gmail.com>
2023-08-08 11:04:47 +0300

    Pass the unit name to be used to create a card above the pdfview

--- app/src/main/java/com/example/comradebuddy/presentation/notes/PdfViewer.kt
+++ app/src/main/java/com/example/comradebuddy/presentation/notes/PdfViewer.kt
@@ -1,15 +1,29 @@
 package com.example.comradebuddy.presentation.notes
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
+import com.example.comradebuddy.AppNavigation
+import com.example.comradebuddy.presentation.ui.AppTopAppBar
+import com.example.comradebuddy.presentation.ui.BottomNavBar
+import com.example.comradebuddy.presentation.ui.BottomNavItem
 
 // PDf view Composable
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun PdfDocViewer(
     fileName: String,
+    unitName: String,
     pdfViewModel: PdfViewModel = viewModel()
 ) {
-    // Call the pdfViewer composable function in the viewModel
-    pdfViewModel.PdfViewer(fileName)
+        // Call the pdfViewer composable function in the viewModel
+        pdfViewModel.PdfViewer(fileName, unitName )
 
 }
