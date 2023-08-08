aaed6e57c2334c32433b73957c3ef3706277cd99
KingsMunene <munenekings2@gmail.com>
2023-08-08 11:01:51 +0300

    Pass a scaffold to the unitsList

--- app/src/main/java/com/example/comradebuddy/presentation/notes/UnitsList.kt
+++ app/src/main/java/com/example/comradebuddy/presentation/notes/UnitsList.kt
@@ -5,31 +5,76 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.example.comradebuddy.AppNavigation
 import com.example.comradebuddy.data.CourseUnit
 import com.example.comradebuddy.data.DataManager
+import com.example.comradebuddy.presentation.ui.AppTopAppBar
+import com.example.comradebuddy.presentation.ui.BottomNavBar
+import com.example.comradebuddy.presentation.ui.BottomNavItem
+import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun UnitsList(
     units: List<CourseUnit>,
-    onUnitClicked: (String) -> Unit
+    onUnitClicked: (String, String) -> Unit,
+    navController: NavController,
 ) {
-    LazyColumn(
-        verticalArrangement = Arrangement.spacedBy(8.dp),
-        contentPadding = PaddingValues(vertical = 16.dp),
-        modifier = Modifier.padding(8.dp)
-    )
-    {
-        items(
-            units
-        ){unit ->
-            UnitItem(
-                itemName = unit.unitName,
-                onItemClicked = {onUnitClicked(unit.fileName)}
+    val scope = rememberCoroutineScope()
+    // Maintain the state of the drawer
+    Scaffold(
+        topBar = {
+            AppTopAppBar(
+                onIconButtonClick = {
+                },
+                false
             )
+        },
+        bottomBar = {
+            BottomNavBar(
+                navController = navController ,
+                items = listOf(
+                    BottomNavItem(
+                        name ="Home",
+                        route = AppNavigation.HOME.name,
+                        icon = Icons.Default.Home
+                    )
+                ),
+                onItemClick = {
+                    navController.popBackStack()
+                    navController.navigate(it.route)
+                }
+            )
+        }
+
+    ) { padding->
+
+        LazyColumn(
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+            contentPadding = PaddingValues(vertical = 16.dp),
+            modifier = Modifier.padding(padding)
+        )
+        {
+            items(
+                units
+            ) { unit ->
+                UnitItem(
+                    itemName = unit.unitName,
+                    onItemClicked = { onUnitClicked(unit.fileName, unit.unitName) }
+                )
+            }
         }
     }
 }
@@ -39,5 +84,5 @@ fun UnitsList(
 fun Unit() {
 
 //    var courses = getUnits(DataManager.courses)
-    UnitsList(DataManager.courses[0].units, onUnitClicked = {})
+    //UnitsList(DataManager.courses[0].units, onUnitClicked = {})
 }
